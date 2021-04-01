2bfc26ff13f438175c59552282e1a0ce87ebc430
faiysal96 <66429838+faiysal96@users.noreply.github.com>
2021-04-01 15:43:30 -0400

    Add files via upload

---
+++ LoginTest.java
@@ -0,0 +1,22 @@
+package maven_package;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void workshopmethod() {
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium-java-3.141.59\\geckodriver-v0.27.0-win64\\geckodriver.exe");
+		//WebDriver w = new ChromeDriver();
+		WebDriver w = new FirefoxDriver();
+		w.get("http://localhost/new/Web.php");
+		w.manage().window().maximize();
+		w.findElement(By.xpath("/html/body/div[1]/form/a[1]")).click();
+		w.findElement(By.xpath("/html/body/div/form/center/input[1]")).sendKeys("john");
+		w.findElement(By.xpath("/html/body/div/form/center/input[2]")).sendKeys("123");
+		w.findElement(By.id("login_btn")).click();
+		
+	}
+}
---
+++ OpenwebsiteTest.java
@@ -0,0 +1,21 @@
+package maven_package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class OpenwebsiteTest {
+	
+	@Test
+	public void workshopmethod() {
+		System.setProperty("webdriver.gecko.driver", "C:\\selenium-java-3.141.59\\geckodriver-v0.27.0-win64\\geckodriver.exe");
+		//WebDriver w = new ChromeDriver();
+		WebDriver w = new FirefoxDriver();
+		w.get("http://localhost/new/Web.php");
+		w.manage().window().maximize();
+		
+		
+		
+	}
+
+}
